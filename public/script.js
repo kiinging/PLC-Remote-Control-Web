eda5b3621451300c5da4936e29439ec67d326b3d
wongkiinging <92498689+kiinging@users.noreply.github.com>
2025-05-01 21:32:25 +0800

    not working

--- public/script.js
+++ public/script.js
@@ -83,8 +83,8 @@ async function fetchTrendData() {
                 display: true,
                 text: 'MV (%)'
               },
-              min: 0,
-              max: 100,
+                min: 0,
+                max: 100,
               grid: {
                 drawOnChartArea: false,
               }
@@ -96,10 +96,6 @@ async function fetchTrendData() {
       chart.data.labels = labels;
       chart.data.datasets[0].data = pvData;
       chart.data.datasets[1].data = mvData;
-      chart.options.scales.y.min = 0;
-      chart.options.scales.y.max = 150;
-      chart.options.scales.y1.min = 0;
-      chart.options.scales.y1.max = 100;
       chart.update();
     }
 
