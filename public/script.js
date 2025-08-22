0e12c6d7b0840396bed481638205cbf09d318b4a
wongkiinging <92498689+kiinging@users.noreply.github.com>
2025-08-22 23:27:34 +0800

    indicator

--- public/script.js
+++ public/script.js
@@ -23,7 +23,7 @@ async function fetchInitialParams() {
     updateIndicator("light-indicator", statusData.light === 1);
     updateIndicator("plc-indicator", statusData.plc === 1);
 
-    console.log("Fetched initial setpoint & PID:", { setData, pidData });
+    console.log("Fetched initial setpoint & PID:", { setData, pidData,  statusData });
   } catch (err) {
     console.error("Failed to fetch initial params:", err);
   }
